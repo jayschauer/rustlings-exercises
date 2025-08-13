5aa3f45c056459c33c7c25208766a1099a4c25e6
Jay Schauer <jschauer@umich.edu>
2025-08-13 17:20:24 -0500

    Make pedantic clippy happy

--- exercises/00_intro/intro1.rs
+++ exercises/00_intro/intro1.rs
@@ -1,11 +1,11 @@
 fn main() {
-    println!(r#"       Welcome to...                      "#);
-    println!(r#"                 _   _ _                  "#);
-    println!(r#"  _ __ _   _ ___| |_| (_)_ __   __ _ ___  "#);
-    println!(r#" | '__| | | / __| __| | | '_ \ / _` / __| "#);
-    println!(r#" | |  | |_| \__ \ |_| | | | | | (_| \__ \ "#);
-    println!(r#" |_|   \__,_|___/\__|_|_|_| |_|\__, |___/ "#);
-    println!(r#"                               |___/      "#);
+    println!(r"       Welcome to...                      ");
+    println!(r"                 _   _ _                  ");
+    println!(r"  _ __ _   _ ___| |_| (_)_ __   __ _ ___  ");
+    println!(r" | '__| | | / __| __| | | '_ \ / _` / __| ");
+    println!(r" | |  | |_| \__ \ |_| | | | | | (_| \__ \ ");
+    println!(r" |_|   \__,_|___/\__|_|_|_| |_|\__, |___/ ");
+    println!(r"                               |___/      ");
     println!();
     println!("This exercise compiles successfully. The remaining exercises contain a compiler");
     println!("or logic error. The central concept behind Rustlings is to fix these errors and");
